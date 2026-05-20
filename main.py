9ddea57eb436c3ab518bec1e567b93c3883f2904
unknown <mutkhaaaa@gmail.com>
2026-05-20 17:49:01 +0700

    port

--- main.py
+++ main.py
@@ -154,4 +154,7 @@ async def predict(file: UploadFile = File(...)):
             "error": str(e),
             "status": "failed"
         }
-
+    
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))  # Ambil PORT dari Railway
+    uvicorn.run(app, host="0.0.0.0", port=port)
