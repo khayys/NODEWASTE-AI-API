9ac3478e9a903729120bde039ca5d7b95f0fe453
unknown <mutkhaaaa@gmail.com>
2026-05-23 19:23:47 +0700

    hf

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def get_recycling_tips(waste_type):
 
     try:
 
-        API_URL = "https://api-inference.huggingface.co/models/google/flan-t5-large"
+        API_URL = "https://router.huggingface.co/hf-inference/models/google/flan-t5-large"
 
         headers = {
             "Authorization": f"Bearer {API_KEY}"
